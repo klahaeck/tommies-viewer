dca73d2f3c4d8795823ef75ab348ab76b6d7bfcf
Kla Haeck <kla@zsh.email>
2025-09-05 14:12:49 -0500

    updates

--- app/types/cesium.d.ts
+++ app/types/cesium.d.ts
@@ -1,12 +1,4 @@
 declare module 'cesium' {
-  export interface Cartesian3 {
-    static fromDegrees(longitude: number, latitude: number, height?: number): Cartesian3;
-  }
-
-  export interface Math {
-    static toRadians(degrees: number): number;
-  }
-
   export interface Camera {
     setView(options: {
       destination: Cartesian3;
@@ -55,18 +47,6 @@ declare module 'cesium' {
     skyAtmosphere?: any;
   }
 
-  export interface Cesium3DTileset {
-    readyPromise?: Promise<Cesium3DTileset>;
-    ready: boolean;
-    showCreditsOnScreen: boolean;
-    maximumScreenSpaceError: number;
-    maximumMemoryUsage: number;
-    cullWithChildrenBounds: boolean;
-    dynamicScreenSpaceError: boolean;
-    dynamicScreenSpaceErrorDensity: number;
-    dynamicScreenSpaceErrorFactor: number;
-    dynamicScreenSpaceErrorHeightFalloff: number;
-  }
 
   export interface Primitives {
     add(tileset: Cesium3DTileset): Cesium3DTileset;
@@ -77,14 +57,11 @@ declare module 'cesium' {
     primitives: Primitives;
   }
 
-  export interface Viewer {
+  export class Viewer {
+    constructor(container: HTMLElement, options?: ViewerOptions);
     camera: Camera;
     scene: Scene;
     zoomTo(target: any): void;
-  }
-
-  export class Viewer {
-    constructor(container: HTMLElement, options?: ViewerOptions);
   }
 
   export class Cesium3DTileset {
@@ -100,6 +77,16 @@ declare module 'cesium' {
       dynamicScreenSpaceErrorHeightFalloff?: number;
       [key: string]: any; // Allow additional properties
     });
+    readyPromise?: Promise<Cesium3DTileset>;
+    ready: boolean;
+    showCreditsOnScreen: boolean;
+    maximumScreenSpaceError: number;
+    maximumMemoryUsage: number;
+    cullWithChildrenBounds: boolean;
+    dynamicScreenSpaceError: boolean;
+    dynamicScreenSpaceErrorDensity: number;
+    dynamicScreenSpaceErrorFactor: number;
+    dynamicScreenSpaceErrorHeightFalloff: number;
   }
 
   export class Cartesian3 {
